cf2452ae598068689daad4b27c8b7bfe86eabafb
JanBryn <janbwiggels@gmail.com>
2024-09-17 09:20:34 +0200

    Initial Commit

---
+++ BugetTracker.py
@@ -0,0 +1,14 @@
+"""
+A buget tracker app that will help monitor their spending, create and update
+expense categories, track income, and calculate remaining
+funds based on their budget.
+"""
+# Imports
+
+"""
+Database handleling will check if its created and if not will make one
+"""
+
+"""
+Main menu UI and main loop that will run
+"""
